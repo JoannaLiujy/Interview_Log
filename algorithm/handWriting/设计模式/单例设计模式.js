c58a9d54066b95efb4d824eb2c54913b55381064
baoer <liujy_joanna@163.com>
2022-02-12 10:59:55 +0800

    单例模式

--- algorithm/handWriting/设计模式/单例设计模式.js
+++ algorithm/handWriting/设计模式/单例设计模式.js
@@ -1,11 +1,40 @@
-let namespace = (function () {
-  let fn = function () {
+// let namespace = (function () {
+//   let fn = function () {
+//     // ...
+//   };
+//   return {
+//     name: 'xxx',
+//     fun: fn
+//   }
+// })();
+// console.log(namespace.name);
+// namespace.fun();
+
+/*
+ * 公共模块：util 
+ */
+let util = (function () {
+  let queryElement = function () {
     // ...
-  };
+  }
   return {
-    name: 'xxx',
-    fun: fn
+    queryElement
   }
 })();
-console.log(namespace.name);
-namespace.fun();
+/*
+ * 独立某模块：part1 
+ */
+let part1 = (function () {
+  let specialFunc = util.queryElement('box');
+  let show = function () {
+    // ...
+  }
+
+  return {
+    init: function () {
+      specialFunc();
+      show();
+    }
+  }
+})();
+part1.init();
