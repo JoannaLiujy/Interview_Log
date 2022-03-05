1c677ab6e1529525d6193958e75cd564cf9cc183
baoer <liujy_joanna@163.com>
2022-03-05 10:33:44 +0800

    更改

--- algorithm/hot100/_206. 反转链表.js
+++ algorithm/hot100/_206. 反转链表.js
@@ -11,15 +11,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var reverseList = function(head) {
-   let prev = null
-   let cur = head
-   let temp 
-   while(cur) {
-     temp = cur.next
-     cur.next = prev
-     prev = cur
-     cur = temp
-   }
-   return prev
-};
+const reverseLink = function (head) {
+  let prev = null;
+  let curr = head;
+  let next = null;
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
